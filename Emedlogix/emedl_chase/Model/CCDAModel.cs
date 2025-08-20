d3c4e5c64f8ca0e1b97867bba588c8a77dfd2949
harish <mdharish@emedlogix.com>
2025-08-20 20:43:28 +0530

    api

---
+++ Emedlogix/emedl_chase/Model/CCDAModel.cs
@@ -0,0 +1,10 @@
+﻿namespace emedl_chase.Model
+{
+    public class CCDAHL7Model
+    {
+        public string section { get; set; }
+        public string Sentence { get; set; }
+        public string DateOfEncounter { get; set; }
+        public int PageNo { get; set; }
+    }
+}
