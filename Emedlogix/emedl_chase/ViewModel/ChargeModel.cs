89dac9aada2671f3b0d47ea2a210285a497b5e90
harish <mdharish@emedlogix.com>
2025-10-12 13:50:00 +0530

    DELERDS ADD

---
+++ Emedlogix/emedl_chase/ViewModel/ChargeModel.cs
@@ -0,0 +1,16 @@
+﻿namespace emedl_chase.ViewModel
+{
+    public class ChargeModel
+    {
+    public int Id { get; set; }
+    public string patient_name { get; set; }
+    public string patient_id { get; set; }
+    public string cpt_code { get; set; }
+    public string practice { get; set; }
+    public int? claim_id { get; set; }
+    public int? encounter_id { get; set; }
+    public DateTime? dos { get; set; }
+    public DateTime? claim_date { get; set; }
+
+    }
+}
