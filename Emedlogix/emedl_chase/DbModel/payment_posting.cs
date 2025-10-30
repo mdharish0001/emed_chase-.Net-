9e7e2bb89916a3f432410dded32fa34b1bd6c765
harishemed <mdharish@emedlogix.com>
2025-10-30 21:58:50 +0530

    files

---
+++ Emedlogix/emedl_chase/DbModel/payment_posting.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emedl_chase.DbModel
+{
+    public class payment_posting
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+      
+        public string? patient_id { get; set; }
+        public string? patient_name { get; set; }
+
+        [Column(TypeName = "timestamp without time zone")]
+        public DateTime? dos { get; set; }
+        public string? cpt { get; set; }
+        public string? claim_id { get; set; }
+        public string? encounter_id { get; set; }
+        public string? provider { get; set; }
+        public string? practice { get; set; }
+        public string? payment_method { get; set; }
+
+        [Column(TypeName = "timestamp without time zone")]
+        public DateTime? claim_date { get; set; }
+        [Column(TypeName = "timestamp without time zone")]
+        public DateTime? payment_date { get; set; }
+        public string? facility{ get; set; }
+        public string? insurance { get; set; }
+        public int org_id { get; set; }
+
+        [Column(TypeName = "timestamp without time zone")]
+        public DateTime? created_on { get; set; }
+        public double? paid_amount { get; set; }
+        public Boolean isdelete { get; set; }
+    
+        
+
+        public int user_id { get; set; }
+        public string? username { get; set; }
+        public string? post_type { get; set; }
+
+    }
+}
---
+++ Emedlogix/emedl_chase/ViewModel/payment_postViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emedl_chase.ViewModel
+{
+    public class payment_postViewModel
+    {
+        public int Id { get; set; }
+        public string patient_name { get; set; }
+        public string patient_id { get; set; }
+        public string cpt_code { get; set; }
+        public string practice { get; set; }
+        public string? claim_id { get; set; }
+        public string encounter_id { get; set; }
+        public DateTime? dos { get; set; }
+        public DateTime? claim_date { get; set; }
+      
+
+    }
+}
