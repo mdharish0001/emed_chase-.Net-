be0758d2ae6e1d45432741ac9d9c9f0de7a585b1
harishemed <mdharish@emedlogix.com>
2025-10-31 20:05:58 +0530

    controller

---
+++ Emedlogix/emedl_chase/Controllers/PaymentPostingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace emedl_chase.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentPostingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
